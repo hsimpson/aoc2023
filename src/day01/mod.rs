7b3b56fadcc859ad7c9f7391b4f2528728daeaae
Daniel Toplak <homer.simpson123@gmx.de>
2023-12-05 01:05:02 +0100

    feat: day01

---
+++ src/day01/mod.rs
@@ -0,0 +1,73 @@
+use crate::utils;
+use std::time::Instant;
+
+pub fn puzzle1() {
+    let start = Instant::now();
+    println!("Day 1, puzzle 1");
+
+    let input = utils::file::read_input("src/day01/input.txt");
+    let lines = input.lines().collect::<Vec<&str>>();
+    let mut sum: u32 = 0;
+
+    for line in lines {
+        let chars = line.chars().collect::<Vec<char>>();
+        let mut digits: Vec<u32> = Vec::new();
+        for c in chars {
+            if c.is_ascii_digit() {
+                digits.push(c.to_digit(10).unwrap());
+            }
+        }
+
+        let first = digits[0];
+        let last = digits[digits.len() - 1];
+        let number = first * 10 + last;
+        sum += number;
+    }
+
+    println!("Sum: {}", sum);
+
+    println!("Time elapsed: {:?}", start.elapsed());
+}
+
+pub fn puzzle2() {
+    let start = Instant::now();
+    println!("Day 1, puzzle 2");
+
+    let input = utils::file::read_input("src/day01/input.txt");
+    let lines = input.lines().collect::<Vec<&str>>();
+    let mut sum: u32 = 0;
+
+    let number_words = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    for line in lines {
+        let mut digits: Vec<u32> = Vec::new();
+
+        for i in 0..line.len() {
+            let c = line.chars().nth(i).unwrap();
+
+            if c.is_ascii_digit() {
+                digits.push(c.to_digit(10).unwrap());
+            } else {
+                for (j, number_word) in number_words.iter().enumerate() {
+                    if line[i..].starts_with(number_word) {
+                        digits.push((j as u32) + 1);
+                        break;
+                    }
+                }
+            }
+        }
+
+        if !digits.is_empty() {
+            let first = digits[0];
+            let last = digits[digits.len() - 1];
+            let number = first * 10 + last;
+            sum += number;
+        }
+    }
+
+    println!("Sum: {}", sum);
+
+    println!("Time elapsed: {:?}", start.elapsed());
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod utils {
+    pub mod file;
+}
+
+mod day01;
+fn main() {
+    println!("Advent of Code 2023");
+    day01::puzzle1();
+    day01::puzzle2();
+}
