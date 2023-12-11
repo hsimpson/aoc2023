9c1921445014acea717c71cdb4ad090bf164938d
Daniel Toplak <homer.simpson123@gmx.de>
2023-12-12 00:48:32 +0100

    feat: day05-2

--- src/day05/mod.rs
+++ src/day05/mod.rs
@@ -19,7 +19,7 @@ impl Map {
     fn map_destination(&self, seed: u64) -> u64 {
         for item in &self.items {
             if seed >= item.source_range_start
-                && seed <= (item.source_range_start + item.range_length)
+                && seed < (item.source_range_start + item.range_length)
             {
                 let offset = seed - item.source_range_start;
                 return item.destination_range_start + offset;
@@ -112,11 +112,82 @@ pub fn puzzle1() {
     println!("Time elapsed: {:?}", start.elapsed());
 }
 
+#[derive(Debug)]
+struct Seed {
+    start: u64,
+    length: u64,
+}
+
+fn parse_range_seeds(raw_seeds: Vec<u64>) -> Vec<Seed> {
+    let mut seeds: Vec<Seed> = Vec::new();
+
+    let mut i = 0;
+    while i < raw_seeds.len() {
+        let start = raw_seeds[i];
+        let length = raw_seeds[i + 1];
+        seeds.push(Seed { start, length });
+
+        i += 2;
+    }
+
+    seeds.sort_by(|a, b| a.start.cmp(&b.start));
+
+    seeds
+}
 pub fn puzzle2() {
     let start = Instant::now();
     println!("Day 5, puzzle 2");
 
-    //let input = utils::file::read_input("src/day05/test.txt");
+    let input = utils::file::read_input("src/day05/input.txt");
+    let lines = input.lines().collect::<Vec<&str>>();
+
+    let mut seeds: Vec<u64> = Vec::new();
+    let mut maps: Vec<Map> = Vec::new();
+
+    let mut i = 0;
+
+    while i < lines.len() {
+        let line = lines[i];
+        if line.is_empty() {
+            i += 1;
+            continue;
+        }
+
+        if line.starts_with("seeds:") {
+            let seed_strings = lines[i].split(':').collect::<Vec<&str>>()[1];
+            seeds = seed_strings
+                .split_whitespace()
+                .map(|s| s.parse::<u64>().unwrap())
+                .collect::<Vec<u64>>();
+            i += 1;
+            continue;
+        }
+
+        let first_char = line.chars().next().unwrap();
+        if first_char.is_alphabetic() {
+            let map = parse_map(&lines, &mut i);
+            maps.push(map);
+        }
+
+        i += 1;
+    }
+
+    let mut lowest_location = u64::MAX;
+
+    let range_seeds = parse_range_seeds(seeds);
+
+    for range_seed in range_seeds {
+        println! {"{:?}", range_seed};
+        for seed in range_seed.start..(range_seed.start + range_seed.length) {
+            let mut destination = seed;
+            for map in &maps {
+                destination = map.map_destination(destination);
+            }
+            lowest_location = lowest_location.min(destination);
+        }
+    }
+
+    println!("Lowest location: {}", lowest_location);
 
     println!("Time elapsed: {:?}", start.elapsed());
 }
